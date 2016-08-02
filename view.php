a9aa2573b3b5d50185e0248c1e730335ba41a99f
raoul2000 <manu34@free.fr>
2016-08-02 17:37:34 +0200

    apply style

--- view.php
+++ view.php
@@ -25,6 +25,8 @@ $timezone = isset($config['timezone']) && ! empty($config['timezone'])
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <link href="design/style.css" rel="stylesheet" type="text/css">
 </head>
@@ -72,8 +74,10 @@ $timezone = isset($config['timezone']) && ! empty($config['timezone'])
                     ?>
 
                       <div class="col-md-4">
-                        <img src="<?= $fileRelativePath ?>" class="img-responsive clickable">
-                        <h4 class="text-center"><?= $dateFmt ?></h4>
+                        <div class="thumbnail">
+                          <img src="<?= $fileRelativePath ?>" class="img-responsive clickable">
+                          <h4 class="text-center"><?= $dateFmt ?></h4>
+                        </div>
                       </div>
 
                     <?php
