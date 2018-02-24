b6c215ff8f96642caf8c781d54e0611a788647e5
raoul2000 <manu34@free.fr>
2018-02-24 15:18:47 +0100

    fix post-install step

--- deploy/post-install.php
+++ deploy/post-install.php
@@ -1,18 +1,25 @@
 <?php
 
 
-$autPwdFilename= realpath('explorer/lib') . '/.htpasswd';
-$htaccessFilename= realpath('explorer') . '/.htaccess';
+///////////////////////////////////////////////////////////////////////////////
+// Protect Explorer with Basic HTTP Authentication
+// UPDATE USERNAMPE AND PASSWORD //////////////////////////////////////////////
+$USERNAME="admin";
+$PASSWORD="123456";
+///////////////////////////////////////////////////////////////////////////////
+
+$autPwdFilename= realpath('../explorer/lib') . '/.htpasswd';
+$htaccessFilename= realpath('../explorer') . '/.htaccess';
 
 if( ! file_exists($autPwdFilename)  ) {
   echo ".htpasswd file nout found in ../lib : creating ...<br/>";
   touch($autPwdFilename);
-  $clearTextPassword = '123456';
+  $clearTextPassword = $PASSWORD;
 
   // Encrypt password
   //$password = crypt($clearTextPassword, base64_encode($clearTextPassword));
   $password = base64_encode(sha1($clearTextPassword, true));
-  file_put_contents($autPwdFilename, "admin:{SHA}$password");
+  file_put_contents($autPwdFilename, "$USERNAME:{SHA}$password");
 } else {
   echo ".htpasswd file exist : $autPwdFilename (not modified)<br/>";
 }
